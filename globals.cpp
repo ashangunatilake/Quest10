c99205fc44c1ef4444d6bff6c61afdd800c55da4
ashangunatilake <manuruddhaashan@gmail.com>
2024-01-11 01:35:06 +0530

    Delete globals.cpp

--- globals.cpp
+++
@@ -1,7 +0,0 @@
-#ifndef GLOBALS_H  // Header guard to prevent multiple inclusions
-#define GLOBALS_H
-
- // Declaration of the global array
- extern char arrChar[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T' };
- extern int arrInt[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
-#endif
